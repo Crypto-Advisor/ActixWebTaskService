0649b5172e2f170e28f2f9c186564de653d76b7e
Adrien <adriennavarre@hotmail.com>
2022-09-27 20:53:02 -0500

    progress

--- src/api/task.rs
+++ src/api/task.rs
@@ -10,3 +10,13 @@ use actix_web::{
     http::{header::ContentType, StatusCode}
 };
 use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct TaskIdentifier {
+    task_global_id: String,
+}
+
+#[get("/task/{task_global_id}")]
+pub async fn get_task(task_identifier: Path<TaskIdentifier>) -> Json<String> {
+    return Json(task_identifier.into_inner().task_global_id);
+}
